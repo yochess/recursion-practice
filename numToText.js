925ed44b711adfef9ae4a90fe10cccf27e07adee
Daham Drake Wang <drakewang@gmail.com>
2016-01-26 22:36:26 -0800

    Corrected issue with spacings and 0

--- numToText.js
+++ numToText.js
@@ -28,22 +28,42 @@ for each word,
 
 var numToText = function(str) {
 	var lookupTable = {
-    0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+    1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
     6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
-    11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
-    16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+    11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 
+    15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 
+    19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 
+    50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'
   };
 
+  var bigNumTable = ["thousand", "million", "billion", "trillion", "zillion"];
+
   var numToWord = function(num) {
-    if(lookupTable[num]) {
+    var rest;
+    if(num*1 === 0) {
+      return "";
+    } else if(lookupTable[num]) {
       return lookupTable[num];
-    }
+    } else if(num > 20 && num < 100) {
+      var tens = Math.floor(num/10) * 10;
+      rest = num - tens;
+      return lookupTable[tens] + "-" + lookupTable[rest];
+    } else if(num >= 100 && num < 1000) {
+      var base = Math.floor(num/100);
+      rest = num - (base*100);
+      return lookupTable[base] + " hundred" + numToWord(rest);
+    } 
   };
 
   var words = str.split(" ");
-  words.forEach(function(wordOrNum, index) {
-    if(!isNaN(wordOrNum)) {
-      words[index] = numToWord(wordOrNum);
+  words.forEach(function(word, index) {
+    if(!isNaN(word)) {
+      var num = word*1;
+      if(num === 0) {
+        words[index] = 0;
+      } else {
+        words[index] = numToWord(num);
+      }
     }
   });
 
