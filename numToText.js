def43ab9185f033f347f83993042c6e541ffd92d
Daham Drake Wang <drakewang@gmail.com>
2016-01-26 20:33:11 -0800

    Implemented numToText for nums between 0 and 19

--- numToText.js
+++ numToText.js
@@ -8,6 +8,44 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 
 */
 
-var numToText = function(str) {
+/* Pseudo code
 
+LOOKUP TABLE consists of:
+- numbers corresponding to the word from zero to nineteen
+
+PROCEDURE:
+split the string into an array of words
+for each word, 
+  if it's not a digit then return the word
+  else
+    if num is between 0 and 19 then return its lookup value
+    ------ under construction ------
+    elseif num is between 20 and 99, 
+    elseif
+    --------------------------------
+*/
+
+
+var numToText = function(str) {
+	var lookupTable = {
+    0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+    11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
+    16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+  };
+
+  var numToWord = function(num) {
+    if(lookupTable[num]) {
+      return lookupTable[num];
+    }
+  };
+
+  var words = str.split(" ");
+  words.forEach(function(wordOrNum, index) {
+    if(!isNaN(wordOrNum)) {
+      words[index] = numToWord(wordOrNum);
+    }
+  });
+
+  return words.join(" ");
 };
